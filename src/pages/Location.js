0cc71bf525c24cf29cac3376e41ead6945e8bfe0
firdows <ahamad.jedu@gmail.com>
2017-11-04 17:37:17 +0000

    07 - Load rest data to state and set Data Table with Pagination

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Table,Pagination} from 'antd';
 
 class Location extends Component {
     
@@ -7,12 +8,16 @@ class Location extends Component {
         dataSource:{}
     }
     
-    loadData(){
-        const url = 'https://yii2-react-firdows.c9users.io/location/';
+    loadData(page=null){
+        const paramPage = (page===null)?``:`?page=${page}`;
+        const url = `https://yii2-react-firdows.c9users.io/location${paramPage}`;
         axios.get(url).then(res=>{
             //console.log(res);
-            if(res.status == 200){
+            if(res.status === 200){
                 console.log(res.data);
+                this.setState({
+                    dataSource:res.data
+                })
             }else{
                 alert(`Can not load ${url}`);
             }
@@ -20,12 +25,35 @@ class Location extends Component {
     }    
     
     
+    onChangePage = (page) => {
+        //console.log(page);
+        this.loadData(page);
+    }
+    
+    componentWillMount(){
+        this.loadData();
+    }
+  
   render() {
+      const columns =[
+          { title: 'PROVINCE_ID', dataIndex: 'PROVINCE_ID', rowKey:'PROVINCE_ID'},
+          { title: 'PROVINCE_CODE', dataIndex: 'PROVINCE_CODE', },
+          { title: 'PROVINCE_NAME', dataIndex: 'PROVINCE_NAME', render: text => <a href="{text}">{text}</a>,}
+          ];
+          
     return (
       <div>
         <h1>Location</h1>
         <button onClick = {() => this.loadData()}>Load Data</button>
-        
+        <Table 
+            columns={columns} 
+            dataSource={this.state.dataSource.data} 
+            pagination={false}
+        />
+        <Pagination 
+        defaultCurrent={1} 
+        total={this.state.dataSource.totalCount} 
+        onChange={this.onChangePage}/>
       </div>
     );
   }
